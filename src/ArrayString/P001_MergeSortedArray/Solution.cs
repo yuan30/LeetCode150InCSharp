465e218b9323071591f565fb3249018f5f6df9c8
yuan30 <lzyzlzlzyz505@gmail.com>
2026-07-10 00:51:42 +0800

    P001 Solved w/ Help

---
+++ src/ArrayString/P001_MergeSortedArray/Solution.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for the merged array
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // If there are remaining elements in nums2, copy them
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    public static void Main(string[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int m = 3;
+        int[] nums2 = { 2, 5, 6 };
+        int n = 3;
+
+        solution.Merge(nums1, m, nums2, n);
+
+        Console.WriteLine(string.Join(", ", nums1)); // Output: 1, 2, 2, 3, 5, 6
+    }
+}
